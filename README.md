76bb30204bcb9a7bde5fba2415932e31e1d2e7a0
williamwsaul <136658313+williamwsaul@users.noreply.github.com>
2023-06-30 02:21:42 -0500

    Initial commit

---
+++ README.md
@@ -0,0 +1,21 @@
+# William Saul - Personal Website
+## Written with HTML, CSS, & JavaScript
+
+This is a website to host my compositions and other related materials. I submitted this as my final
+project for an introductory coding class in 2023. I am a novice in computer programming, but my objectives are 
+simple. Please contact me here: https://williamwsaul.github.io/williamsaul.com/contact.html
+
+### Project Objectives:
+* Host mp3s, images, pdfs, and text.
+* Create an aesthetically compelling experience for users.
+* Provide contact information with minimal spam-botting.
+* Embed Twitter essays onto research page.
+
+### Potential Issues:
+* Not scaled for mobile devices yet - currently unusable.
+* Implimented encrypted pdf workaround instead of contact form.
+* Embedded Tweets don't load with Brave sheilds/ad blocking.
+
+Much appreciated,
+
+William
---
+++ comp_script.js
@@ -0,0 +1,74 @@
+function openModal(title) {
+    let modal = document.getElementById("modal")
+    let modalTitle = document.getElementById("modal-title")
+    let modalDescription = document.getElementById("modal-description")
+    let modalAudio = document.getElementById("modal-audio")
+    let modalLink_Bpt1 = document.getElementById("modalLink_Bpt1")
+    let modalLink_Bpt2 = document.getElementById("modalLink_Bpt2")
+    let modal_image = document.getElementById("modal_image")
+  
+    // For future reference, replace these with unique details:
+    if (title == "electroacoustic_1") {
+      modalTitle.textContent = "Untitled"
+      modalDescription.textContent = "Coming Soon - No recording available"
+      modalAudio.style = "display: none"
+      modalAudio.style = "display: none"
+      modalLink_Bpt1.style = "display: none"
+      modalLink_Bpt2.style = "display: none"
+      modal_image.src = "none"
+      modal_image.style.display = "none"
+    } else if (title == "orchestra_1") {
+      modalTitle.textContent = "Two Minutes of Spring"
+      modalDescription.textContent = "A short piece for chamber orchestra, dedicated to my wonderful mother & friend; Kellie Saul. \"A slightly somber take on Spring, with hints of frolic.\""
+      modalAudio.style = "display: none"
+      modalAudio.style = "display: auto"
+      modalLink_Bpt1.style = "display: none"
+      modalLink_Bpt2.style = "display: none"
+      modal_image.src = "none"
+      modal_image.style.display = "none"
+    } else if (title == "chamber_2") {
+      modalTitle.textContent = "Cape Elizabeth"
+      modalDescription.textContent = "A four movement string quartet I wrote & recorded in 2021. Credits: Violin I - Lucia Di Blasio Scott, Violin II - Nicole d'Oliveira, Viola - Karin Gaertner, Cello - Jessica Scott, Score Editor - John Walton."
+      modalAudio.src = "audio/Cape_Elizabeth.mp3"
+      modalAudio.style = "display: auto"
+      modalLink_Bpt1.style = "display: none"
+      modalLink_Bpt2.style = "display: none"
+      modal_image.src = "images/Cape Elizabeth.jpg"
+      modal_image.style.display = "inline-block"
+    } else if (title == "other_1") {
+        modalTitle.textContent = "Beginnings Pt. 2"
+        modalDescription.textContent = "An early collection of music I wrote between 2021 & 2022."
+        modalAudio.style = "display: none"
+        modalLink_Bpt1.style = "display: none"
+        modalLink_Bpt2.style = "display: auto"
+        modal_image.src = "images/Beginnings Pt. 2.jpeg"
+        modal_image.style.display = "inline-block"
+      } else if (title == "other_2") {
+        modalTitle.textContent = "Beginnings Pt. 1"
+        modalDescription.textContent = "An early collection of music I wrote between 2018 & 2020."
+        modalAudio.style = "display: none"
+        modalLink_Bpt1.style = "display: auto"
+        modalLink_Bpt2.style = "display: none"
+        modal_image.src = "images/Beginnings Pt. 1.jpeg"
+        modal_image.style.display = "inline-block"
+      }
+    
+ modal.style.display = "block"
+  }
+  
+  function closeModal() {
+    let modal = document.getElementById("modal")
+    let modalAudio = document.getElementById("modal-audio")
+  
+    modal.style.display = "none"
+    modalAudio.pause()
+    modalAudio.currentTime = 0
+  }
+  
+  window.onclick = function (event) {
+    let modal = document.getElementById("modal")
+  
+    if (event.target == modal) {
+      closeModal()
+    }
+  }
