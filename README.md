32ac03166f3b0a2dee28e6e865394cb2d897441c
williamwsaul <136658313+williamwsaul@users.noreply.github.com>
2023-07-29 19:07:04 -0500

    williamwsaul.com V2!
    Version two makes big improvements, adding: hamburger menu for mobile, slideshow/photo gallery archive toggle, more photos, and biography.
    
    Other changes include: social media icon replacements, improved styling on every page, "research.html" to "writings.html," section for research essays/twitter profile, and replaced encrypted pdf email with image form (a little less secure, but much more convenient).

--- README.md
+++ README.md
@@ -2,18 +2,14 @@
 ## Written with HTML, CSS, & JavaScript
 
 This is a website to host my compositions and other related materials. I submitted this as my final
-project for an introductory coding class in 2023. I am a novice in computer programming, but my objectives are 
-simple. Please contact me here: https://williamsaul.com/contact
+project for an introductory coding class in 2023. Please contact me here: https://williamsaul.com/contact
 
 ### Project Objectives:
 * Host mp3s, images, pdfs, and text.
 * Create an aesthetically compelling experience for users.
 * Provide contact information with minimal spam-botting.
-* Embed Twitter essays onto research page.
 
 ### Potential Issues:
-* Not scaled for mobile devices yet - currently unusable.
-* Implimented encrypted pdf workaround instead of contact form.
 * Embedded Tweets don't load with Brave sheilds/ad blocking.
 
 Much appreciated,
--- comp_script.js
+++ comp_script.js
@@ -9,7 +9,7 @@ function openModal(title) {
   
     // For future reference, replace these with unique details:
     if (title == "electroacoustic_1") {
-      modalTitle.textContent = "Untitled"
+      modalTitle.textContent = "Deforestation"
       modalDescription.textContent = "Coming Soon - No recording available"
       modalAudio.style = "display: none"
       modalAudio.style = "display: none"
@@ -26,27 +26,36 @@ function openModal(title) {
       modalLink_Bpt2.style = "display: none"
       modal_image.src = "none"
       modal_image.style.display = "none"
-    } else if (title == "orchestra_1") {
-      modalTitle.textContent = "Two Minutes of Spring"
-      modalDescription.textContent = "A short piece for chamber orchestra, dedicated to my wonderful mother & friend; Kellie Saul. \"A slightly somber take on Spring, with hints of frolic.\""
+    } else if (title == "orchestral_1") {
+      modalTitle.textContent = "Spring Suite"
+      modalDescription.textContent = "Coming Soon - No recording available"
       modalAudio.style = "display: none"
-      modalAudio.style = "display: auto"
+      modalAudio.style = "display: none"
       modalLink_Bpt1.style = "display: none"
       modalLink_Bpt2.style = "display: none"
       modal_image.src = "none"
       modal_image.style.display = "none"
-    } else if (title == "chamber_2") {
+    } else if (title == "chamber_1") {
       modalTitle.textContent = "Cape Elizabeth"
-      modalDescription.textContent = "A four movement string quartet I wrote & recorded in 2021. Credits: Violin I - Lucia Di Blasio Scott, Violin II - Nicole d'Oliveira, Viola - Karin Gaertner, Cello - Jessica Scott, Score Editor - John Walton."
+      modalDescription.textContent = "An early attempt at a four movement string quartet, recorded in 2021. Credits to Lucia Di Blasio Scott (Violin I), Nicole d'Oliveira (Violin II), Karin Gaertner (Viola), Jessica Scott (Cello), & John Walton (Score Editor)."
       modalAudio.src = "audio/Cape_Elizabeth.mp3"
       modalAudio.style = "display: auto"
       modalLink_Bpt1.style = "display: none"
       modalLink_Bpt2.style = "display: none"
       modal_image.src = "images/Cape Elizabeth.jpg"
       modal_image.style.display = "inline-block"
+    } else if (title == "solo_1") {
+      modalTitle.textContent = "serialist.js"
+      modalDescription.textContent = "Coming Soon - No recording available"
+      modalAudio.style = "display: none"
+      modalAudio.style = "display: none"
+      modalLink_Bpt1.style = "display: none"
+      modalLink_Bpt2.style = "display: none"
+      modal_image.src = "none"
+      modal_image.style.display = "none"
     } else if (title == "other_1") {
         modalTitle.textContent = "Beginnings Pt. 2"
-        modalDescription.textContent = "An early collection of music I wrote between 2021 & 2022."
+        modalDescription.textContent = "A collection of early works, between 2021 & 2022."
         modalAudio.style = "display: none"
         modalLink_Bpt1.style = "display: none"
         modalLink_Bpt2.style = "display: auto"
@@ -54,7 +63,7 @@ function openModal(title) {
         modal_image.style.display = "inline-block"
       } else if (title == "other_2") {
         modalTitle.textContent = "Beginnings Pt. 1"
-        modalDescription.textContent = "An early collection of music I wrote between 2018 & 2020."
+        modalDescription.textContent = "A collection of early works, between 2018 & 2020."
         modalAudio.style = "display: none"
         modalLink_Bpt1.style = "display: auto"
         modalLink_Bpt2.style = "display: none"
